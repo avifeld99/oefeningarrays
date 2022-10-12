3d6ecc2c1382fbafcf77004f8269095cdc60adaf
avifeld99 <avifeld99@gmail.com>
2022-10-12 14:54:59 +0200

    commit init

---
+++ src/be/intecbrussel/MyArrays2.java
@@ -0,0 +1,28 @@
+package be.intecbrussel;
+
+import java.util.Scanner;
+
+public class MyArrays2 {
+
+    public static void main(String[] args) {
+
+
+        Scanner myScan = new Scanner(System.in);
+
+        int[] myArr = new int[10];
+
+        System.out.println("enter you 10 numbers: ");
+
+        for (int index = 0; index < myArr.length; index++) {
+            myArr[index] = myScan.nextInt();
+        }
+
+        for (int index = 0; index < myArr.length - 1; index++) {
+            System.out.println("number " + myArr[index]);
+        }
+
+
+        //...........................
+
+    }
+}
